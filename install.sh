1ebabb035bb49c2142f81d453d868835112d1170
Carter Penterman <carterpenterman@gmail.com>
2021-12-10 16:31:34 -0600

    fix installation script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 make lib -j
 mkdir -p "$1"/include
 mkdir "$1"/lib
-cp -r imageio/include "$1"
-mv imageio/lib/libimageio.so "$1/lib/libimageio.so"
+cp -r include "$1"
+mv lib/libimageio.so "$1/lib/libimageio.so"
